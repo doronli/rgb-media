e6280aa031ff121db221ed67b767c059d8190930
Doron <lisbona107@gmail.com>
2020-10-11 16:30:33 +0300

    finish project

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
 window.addEventListener('load', function() {
     console.log('All assets are loaded');
 });
+
 $("#icon-menu").click(function(){
     $("#navbar").toggle(500);
 });
+
 $("#close-navbar").click(function(){
     $("#navbar").toggle(500);
 });
+
+$("body").on("click",".question",function(){
+    const resposeId = $(this).attr("data-target");
+    $(`#${resposeId}`).toggle(500);
+    $(this).find('i').toggleClass("icon-Hartman-icons_arrow-up-bold");
+})
